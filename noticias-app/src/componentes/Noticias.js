9511438212f86dc7acf2b7d108f738349804a4c7
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-05-10 10:31:37 -0500

    Se agrega animación a categoría, detecta si hay o no imagen

--- noticias-app/src/componentes/Noticias.js
+++ noticias-app/src/componentes/Noticias.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Noticia from './Noticia'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Noticias extends Component {
     render(){
         return(
             <div className="row">
-                { /* ***** Componente: Noticia ***** */}
-                { this.props.noticias.map((noticia) => (
-                    <Noticia
-                                key={noticia.url}
-                                noticia={noticia}
-                    ></Noticia>
-                ))}
+                <TransitionGroup>
+                    { /* ***** Componente: Noticia ***** */}
+                    { this.props.noticias.map((noticia) => (
+                        <CSSTransition
+                                        key={noticia.url}
+                                        classNames="fade"
+                                        timeout={500}
+                        >
+                            <Noticia
+                                        noticia={noticia}
+                            ></Noticia>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
             </div>
         )
     }
